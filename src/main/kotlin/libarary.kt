ffecc58f40e03892ece0ba52acbcd100a2067d88
binthia parveen <biva45456@gmail.com>
2023-12-04 08:56:58 +0600

    create search by auther

--- src/main/kotlin/libarary.kt
+++ src/main/kotlin/libarary.kt
@@ -22,3 +22,35 @@ class Library {
             }
         }
     }
+
+// Binthia parveen Biva
+
+    fun searchByAuthor(author: String) {
+        var isAvailable:Boolean = false
+        for (book in books) {
+            if (!book.isBorrowed && book.author == author) {
+                println("${book.title} by ${book.author} (${book.publicationYear})")
+                isAvailable = true
+                break
+            }
+        }
+        if (!isAvailable) {
+            println("Not Available book")
+        }
+
+    }
+
+    fun searchByYear(year: Int) {
+        var isAvailable:Boolean = false
+        for (book in books) {
+            if (!book.isBorrowed && book.publicationYear == year) {
+                println("${book.title} by ${book.author} (${book.publicationYear})")
+                isAvailable = true
+                break
+            }
+
+        }
+        if (!isAvailable) {
+            println("Not Available book")
+        }
+    }
